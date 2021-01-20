c601214aedd108af4a32c20b2403025764adf5bf
laozhangaidaima <751818302zh@gmail.com>
2021-01-20 21:42:30 +0800

    背题模式

---
+++ pages/remember/index.vue
@@ -0,0 +1,60 @@
+<template>
+  <view class="bg-cyan pages" @click="randomMsg(msgItem)">
+    <view class="padding text-center text-size">{{ title }}</view>
+    <view class="main">
+      {{ msg }}
+    </view>
+  </view>
+</template>
+
+<script>
+import * as dataMsg from "./data";
+
+export default {
+  data() {
+    return {
+      msg: "",
+	  msgItem: [],
+	  title:''
+    };
+  },
+  created() {},
+  onLoad: function (option) {
+	//option为object类型，会序列化上个页面传递的参数
+	this.title = option.title
+    for (const key in dataMsg) {
+      if (key === option.id) {
+        this.msgItem = dataMsg[key];
+        this.randomMsg(dataMsg[key]);
+        return;
+      }
+    }
+  },
+  methods: {
+    randomMsg(data) {
+      let index = Math.floor(Math.random() * data.length);
+      this.msg = data[index];
+    },
+  },
+};
+</script>
+
+<style>
+page {
+  height: 100%;
+}
+.pages {
+  height: 100%;
+}
+
+.main {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 50vh;
+  font-size: 24px;
+}
+.text-size{
+	font-size: 30px;
+}
+</style>
--- pages/tabbar/index.vue
+++ pages/tabbar/index.vue
@@ -4,11 +4,12 @@
       <view class="nav-list">
         <navigator
           hover-class="none"
-          :url="'/pages/base/index' + '?title=' + item.title + '&id=' + item.id"
+          :url="
+            '/pages/remember/index' + '?title=' + item.title + '&id=' + item.id
+          "
           class="nav-li"
           navigateTo
           :class="'bg-' + item.color"
-          :style="[{ animation: 'show ' + ((index + 1) * 0.2 + 1) + 's 1' }]"
           v-for="(item, index) in elements"
           :key="index"
         >
@@ -28,16 +29,6 @@ export default {
     return {
       elements: [
         {
-          title: "浏览器",
-          id: "browser",
-          color: "cyan",
-        },
-        {
-          title: "服务器",
-          id: "server",
-          color: "blue",
-        },
-        {
           title: "html",
           id: "html",
           color: "purple",
@@ -51,61 +42,6 @@ export default {
           title: "js",
           id: "js",
           color: "pink",
-        },
-        {
-          title: "vue",
-          id: "vue",
-          color: "brown",
-        },
-        {
-          title: "组件化开发",
-          id: "component",
-          color: "red",
-        },
-        {
-          title: "模块化开发",
-          id: "module",
-          color: "green",
-        },
-        {
-          title: "兼容性方案",
-          id: "8",
-          color: "orange",
-        },
-        {
-          title: "面向对象",
-          id: "object",
-          color: "grey",
-        },
-        {
-          title: "执行顺序",
-          id: "12",
-          color: "black",
-        },
-        {
-          title: "事件队列",
-          id: "13",
-          color: "grey",
-        },
-        {
-          title: "mvvm和mvc",
-          id: "mvvm",
-          color: "pink",
-        },
-        {
-          title: "单元测试",
-          id: "test",
-          color: "mauve",
-        },
-        {
-          title: "代码质量",
-          id: "codeReview",
-          color: "olive",
-        },
-        {
-          title: "面试技巧",
-          id: "interView",
-          color: "cyan",
         },
       ],
     };
