d29f0e8cb6e76ac3d29ee4cbe13e2246fbbe7997
laozhangaidaima <751818302zh@gmail.com>
2021-01-23 17:01:36 +0800

    http

--- pages/tabbar/index.vue
+++ pages/tabbar/index.vue
@@ -43,6 +43,11 @@ export default {
           id: "js",
           color: "pink",
         },
+        {
+          title: "http",
+          id: "http",
+          color: "pink",
+        },
       ],
     };
   },
